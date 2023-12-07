cbece2c28bf96369cd836a4c8be8b380b02af74e
dyang886 <dyang886@outlook.com>
2023-12-07 01:46:01 -0800

    implemented electron-builder for custom installer

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, 'assets/logo.ico'),
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
